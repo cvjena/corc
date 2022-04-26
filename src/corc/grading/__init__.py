63217b8c457b73b8f2faf392a5d692c1477f8cf3
Tim Büchner <tim.buechner@uni-jena.de>
2022-04-26 14:25:49 +0200

    Add grading method and subpackage

---
+++ src/corc/grading/__init__.py
@@ -0,0 +1,10 @@
+"""
+Copyright (c) Computer Vision Group - FSU Jena
+
+Author: Tim Büchner
+Email: tim.buechner@uni-jena.de
+"""
+
+__all__ = ["volume_grading"]
+
+from corc.grading.volume_grading import volume_grading
---
+++ src/corc/grading/volume_grading.py
@@ -0,0 +1,37 @@
+"""
+Copyright (c) Computer Vision Group - FSU Jena
+
+Author: Tim Büchner
+Email: tim.buechner@uni-jena.de
+"""
+__all__ = ["volume_grading"]
+
+import numpy as np
+
+
+def volume_grading(curves: np.ndarray, n_curves: int, n_points) -> float:
+    """Compute the CORC volume grading.
+
+    Parameters
+    ----------
+    curves : np.ndarray
+        The curves.
+    n_curves : int
+        The number of curves.
+    n_points : int
+        The number of points per curve.
+
+    Returns
+    -------
+    float
+        The volume grading.
+    """
+    temp_curves = curves.copy().reshape(n_curves, n_points, 3)
+
+    values = []
+    for i in range(n_curves // 2):
+        values.append(
+            np.sum(np.abs(temp_curves[i, :, 2] - temp_curves[-i, :, 2])) / n_points
+        )
+
+    return float(np.mean(values))
